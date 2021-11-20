0513cb9496f49173b572af94db3f40d65456a7a0
Ahmed Ben Romdhane <ahmed.ben.romdhane@outlook.com>
2021-11-20 14:47:33 +0100

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { BuildingComponent } from './pages/building/building.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuildComponent } from './pages/build/build.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'build',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'building/:buildingId',
+    component: BuildingComponent
+  },
+  {
+    path: 'build',
+    component: BuildComponent
+  },
+  {
+    path: 'build/:buildingId',
+    component: BuildComponent
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BuildingComponent } from './pages/building/building.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BuildComponent } from './pages/build/build.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MATERIALS } from './app.material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuildingComponent,
+    HomeComponent,
+    BuildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MATERIALS
   ],
   providers: [],
   bootstrap: [AppComponent]
